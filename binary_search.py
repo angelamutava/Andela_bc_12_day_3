c502c6128809f7334281dacbe8b90a2ba3e4bc26
angieMutava <mutavaangela@yahoo.com>
2016-11-15 17:13:53 +0300

    added some changes to binary search

--- binary_search.py
+++ binary_search.py
@@ -9,37 +9,29 @@ class BinarySearch(list):
     count = 0
     end = len(self.list_of_numbers) - 1
     output_dict = {'count':count, 'index':0 }
-   
-    if value not in self.list_of_numbers:
+    
+    if search_element not in self.list_of_numbers:
       output_dict['index'] = -1
       return output_dict
     else:
-        while True:
-           midpoint = (start + end)/2
-           if self.list_of_numbers[start] == search_element:
-               output_dict['index'] = start
-               return output_dict
-           elif self.list_of_numbers[end] == search_element:
-               output_dict['index'] = end
-               return output_dict
-           elif self.list_of_numbers[midpoint] == search_element:
-               output_dict['index'] == midpoint
-               output_dict['count'] +=1
-           else:
-               if search_element <self.list_of_numbers[midpoint]:
-                   output_dict['count'] += 1
-                   first = midpoint - 1
-                   output_dict['index'] = self.list_of_numbers.index(search_element)
-               else:
-                   output_dict['count'] += 1 
-                   first = midpoint + 1 
-                   output_dict['index'] = self.list_of_numbers.index(search_element)
-  return output_dict                   
-
-                
-
-            
-
-
-
-   
+      midpoint = (start + end)/2
+      if self.list_of_numbers[start] == search_element:
+        output_dict['index'] = start
+        return output_dict
+      elif self.list_of_numbers[end] == search_element:
+        output_dict['index'] = end
+        return output_dict
+      elif self.list_of_numbers[midpoint] == search_element:
+        output_dict['index'] == midpoint
+        output_dict['count'] +=1
+      else:
+        if search_element <self.list_of_numbers[midpoint]:
+          output_dict['count'] += 1
+          first = midpoint - 1
+          output_dict['index'] = self.list_of_numbers.index(search_element)
+          return output_dict 
+        else:
+          output_dict['count'] += 1 
+          first = midpoint + 1 
+          output_dict['index'] = self.list_of_numbers.index(search_element)
+          return output_dict
