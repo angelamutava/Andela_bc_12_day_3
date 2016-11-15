392eb4a4b4a9aaef4a6b818ef875e36c68651e8b
angieMutava <mutavaangela@yahoo.com>
2016-11-15 15:41:25 +0300

    added changes to binary_search.py

--- binary_search.py
+++ binary_search.py
@@ -1,6 +1,6 @@
 class BinarySearch(list):
   def __init__(self, length_of_list, step):
-    self.list_of_numbers = range(0, length_of_list , step)
+    self.list_of_numbers = range(0+step, length_of_list*step + 1  , step)
     list.__init__(self, self.list_of_numbers)
     self.length = len(self.list_of_numbers) - 1
     
@@ -8,8 +8,40 @@ class BinarySearch(list):
     start = 0 
     count = 0
     end = len(self.list_of_numbers) - 1
-    output_dict = {}
-    midpoint = (start + end)/2
+    output_dict = {'count':count, 'index':0 }
+   
+    if value not in self.list_of_numbers:
+      output_dict['index'] = -1
+      return output_dict
+    else:
+        while True:
+           midpoint = (start + end)/2
+           if self.list_of_numbers[start] == search_element:
+               output_dict['index'] = start
+               return output_dict
+           elif self.list_of_numbers[end] == search_element:
+               output_dict['index'] = end
+               return output_dict
+           elif self.list_of_numbers[midpoint] == search_element:
+               output_dict['index'] == midpoint
+               output_dict['count'] +=1
+           else:
+               if search_element <self.list_of_numbers[midpoint]:
+                   count += 1
+                   first = midpoint - 1
+                   output_dict['index'] = self.list_of_numbers.index(search_element)
+               else:
+                   count += 1 
+                   first = midpoint + 1 
+                   output_dict['index'] = self.list_of_numbers.index(search_element)
+return output_dict                   
+
+                
+
+            
+
+
+
     while True:
       if self.list_of_numbers[midpoint] < search_element:
         count +=1
